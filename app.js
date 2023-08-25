4b3442e79f04d29ba69d5fffe29a8b0be014f631
rikhil7 <rikhil.taneja@kalvium.community>
2023-08-25 09:27:37 +0530

    final

--- app.js
+++ app.js
@@ -1,40 +1,62 @@
 var bodyvar = document.body
 
-var consumptionvar = document.getElementById("consumption")
+var consumptionVar = document.getElementById("consumption")
 
-var climatevar = document.getElementById("climate")
+var climateVar = document.getElementById("climate")
 
-var resourcesvar = document.getElementById("resources")
+var resourcesVar = document.getElementById("resources")
 
-var peoplevar = document.getElementById("people")
+var peopleVar = document.getElementById("people")
 
-console.log(consumptionvar)
-console.log(climatevar)
-console.log(resourcesvar)
-console.log(peoplevar)
+console.log(peopleVar);
+console.log(resourcesVar);
+console.log(climateVar);
+console.log(consumptionVar);
 
-var savetext = document.getElementById("save")
+var saveText = document.getElementById("save")
+
 var frame = document.getElementById("frame")
 
-consumptionvar.onclick=()=>{
-    bodyvar.style.background= "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/food-wastage-bg%5B1%5D.png')"
-    bodyvar.style.backgroundSize= "100% 100%"
-    frame.src = "https://www.theworldcounts.com/embed/challenges/107?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
-    savetext.innerHTML = "Do your bit! Shop only what you need,eat only what you need and always save the leftovers"
-    consumptionvar.style.background= "#2EC455"
+consumptionVar.onclick=()=>{
+    bodyvar.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/food-wastage-bg%5B1%5D.png	')"
+    bodyvar.style.backgroundSize="100% 100%"
+    frame.src="https://www.theworldcounts.com/embed/challenges/107?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+    saveText.innerHTML="Do your bit! Shop only what you need,eat only what you need and always save the leftovers."
+    consumptionVar.style.background="#37C658"
+    climateVar.style.background="transparent"
+    resourcesVar.style.background="transparent"
+    peopleVar.style.background="transparent"
 }
 
-climatevar.onclick=()=>{
-    bodyvar.style.background= "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/climate-change-bg%5B1%5D.png')"
-    bodyvar.style.backgroundSize= "100% 100%"
-    frame.src = "https://www.theworldcounts.com/embed/challenges/23?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
-    savetext.innerHTML = "Do your bit! Save trees, use renewable energy sources and prefer to travel green"
-    climatevar.style.background= "#3880FB"
+climateVar.onclick=()=>{
+    bodyvar.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/climate-change-bg%5B1%5D.png')"
+    bodyvar.style.backgroundSize="100% 100%"
+    saveText.innerHTML="Do your bit! Save trees, use renewable energy sources and prefer to travel green"
+    climateVar.style.background="#3B83C8"
+    consumptionVar.style.background="transparent"
+    resourcesVar.style.background="transparent"
+    peopleVar.style.background="transparent"
+    frame.src= "https://www.theworldcounts.com/embed/challenges/23?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
 }
-resourcesvar.onclick=()=>{
-    bodyvar.style.background= "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/natural-resources-bg%5B1%5D.png')"
-    bodyvar.style.backgroundSize= "100% 100%"
-    frame.src = "https://www.theworldcounts.com/embed/challenges/23?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
-    savetext.innerHTML = "Do your bit! Save trees, use renewable energy sources and prefer to travel green"
-    climatevar.style.background= "#3880FB"
+
+resourcesVar.onclick=()=>{
+    bodyvar.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/natural-resources-bg%5B1%5D.png')"
+    bodyvar.style.backgroundSize="100% 100%"
+    saveText.innerHTML="Do your bit! Avoid all things plastic. Believe in Reduce, Reuse and Recycle"
+    resourcesVar.style.background="#E89F51"
+    climateVar.style.background="transparent"
+    consumptionVar.style.background="transparent"
+    peopleVar.style.background="transparent"
+    frame.src= "https://www.theworldcounts.com/embed/challenges/5?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
+}
+
+peopleVar.onclick=()=>{
+    bodyvar.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/poverty-bg%5B1%5D.png')"
+    bodyvar.style.backgroundSize="100% 100%"
+    saveText.innerHTML="Do your bit! Never waste  food.Rather offer it to people or animals who are in need."
+    peopleVar.style.background="#D94854"
+    climateVar.style.background="transparent"
+    resourcesVar.style.background="transparent"
+    resourcesVar.style.background="transparent"
+    frame.src= "https://www.theworldcounts.com/embed/challenges/2?background_color=transparent&color=white&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20"
 }
